f07661405d979bb56b7ad10bf3929ede74351525
Arnaud POINTET <arnaud.pointet@gmail.com>
2018-02-06 13:23:46 +0100

    https

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import { HydraAdmin } from '@api-platform/admin';
 
-export default () => <HydraAdmin entrypoint="http://api.oipnet.info/api"/>;
+export default () => <HydraAdmin entrypoint="https://api.oipnet.info/api"/>;
